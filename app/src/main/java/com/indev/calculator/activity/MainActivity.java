b16d59e651d4dca75a3cb781c9aec8ea505d551c
Evgeniy <evgeniy.d97@gmail.com>
2016-08-07 12:01:57 +0300

    Facebook Button was created with all of functionality.

--- app/src/main/java/com/indev/calculator/activity/MainActivity.java
+++ app/src/main/java/com/indev/calculator/activity/MainActivity.java
@@ -4,15 +4,29 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.indev.calculator.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private LoginButton loginFacebook;
+    private CallbackManager callbackFacebookManager;
+    private final String ERROR_CONNECTION = "You have some error with Internet connection!";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        onFacebookClick();
     }
 
     public void onForgottenPassClick(View v) {
@@ -26,8 +40,31 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    public void onFaceBookClick(View v) {
+    private void onFacebookClick() {
+        callbackFacebookManager = CallbackManager.Factory.create();
+        loginFacebook = (LoginButton) findViewById(R.id.btnFacebook);
+        loginFacebook.registerCallback(callbackFacebookManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
 
+            }
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Toast.makeText(MainActivity.this, ERROR_CONNECTION, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackFacebookManager.onActivityResult(requestCode, resultCode, data);
     }
 
     public void onGoogleClick(View v) {
