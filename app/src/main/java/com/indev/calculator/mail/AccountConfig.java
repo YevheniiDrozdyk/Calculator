656f162fe6f09180f00393db523c1af7664101b4
Eugene <evgeniy.d97@gmail.com>
2017-02-12 14:15:30 +0200

    Update AccountConfig.java

--- app/src/main/java/com/indev/calculator/mail/AccountConfig.java
+++ app/src/main/java/com/indev/calculator/mail/AccountConfig.java
@@ -6,6 +6,6 @@ package com.indev.calculator.mail;
  * @version 1.0 27 Aug 2016
  */
 class AccountConfig {
-    static final String ADMIN_EMAIL = "app.calculator.staff@gmail.com";
-    static final String ADMIN_PASSWORD = "AdminCalc2016";
+    static final String ADMIN_EMAIL = "";
+    static final String ADMIN_PASSWORD = "";
 }
